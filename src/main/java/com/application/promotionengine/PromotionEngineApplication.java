058a4034391791553573d2326c63d7b7b19a6f65
Vivek Subramaniam <tzar123@gmail.com>
2020-09-01 12:08:46 +0530

    adding calcualtion logic

--- src/main/java/com/application/promotionengine/PromotionEngineApplication.java
+++ src/main/java/com/application/promotionengine/PromotionEngineApplication.java
@@ -10,10 +10,8 @@ public class PromotionEngineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PromotionEngineApplication.class, args);
-		System.out.println("please enter the no of elemnet you want in cart");
-		Scanner in = new Scanner(System.in);
-		int numberOfElements = in.nextInt();
-		System.out.println("number of elements are "+numberOfElements);
+		
+		
 	}
 
 }
--- src/main/java/com/application/promotionengine/service/ProductServiceIml.java
+++ src/main/java/com/application/promotionengine/service/ProductServiceIml.java
@@ -1,5 +1,7 @@
 package com.application.promotionengine.service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ProductServiceIml implements ProductService {
@@ -15,10 +17,38 @@ public class ProductServiceIml implements ProductService {
 			skuIDList[i] = in.next();
 		}
 		in.close();
+		
+		int totalPrice = priceClaculation(skuIDList);
+		System.out.println("Total Price for the transaction is : " +totalPrice );
 	}
 	
 	public int priceClaculation(String[] skuIDList) {
+		Map<String,Integer> SkduqauntityMap = new HashMap<String,Integer>();
+		int countA = 0;
+		int countB = 0;
+		int countC = 0;
+		int countD = 0;
+		for (int i = 0; i < skuIDList.length; i++) {
+			if (skuIDList[i].contains("A")) {
+				countA+=1;
+			}
+			else if (skuIDList[i].contains("B")) {
+				countB+= 1;
+			} 
+			else if(skuIDList[i].contains("C")) {
+				countC+= 1;
+			}
+			else if(skuIDList[i].contains("D")) {
+				countD+= 1;
+			}
+			
+		}
+		SkduqauntityMap.put("A", countA);
+		SkduqauntityMap.put("B", countB);
+		SkduqauntityMap.put("C", countC);
+		SkduqauntityMap.put("D", countD);
 		return 0;
 	}
+
 	
 }
